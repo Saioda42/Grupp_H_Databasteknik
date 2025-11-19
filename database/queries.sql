5586f3cc9f23185fcaa4645d658a482e4df355dd
Philip Bulander <Philip@Bulander.se>
2025-11-19 11:12:28 +0100

    har skapa fler queries i queries.sql

---
+++ database/queries.sql
@@ -0,0 +1,43 @@
+-- Aggregering och analys (5 st):
+-- 11. Räkna antal böcker per kategori
+-- 12. Hitta de 5 mest populära böckerna (mest lånade)
+-- 13. Visa medlemmar med flest antal lån
+-- 14. Beräkna genomsnittligt antal dagar mellan lånedatum och återlämning
+-- 15. Visa hur många böcker som är försenade (due_date passerat men inte återlämnade)
+
+SELECT 
+    category, 
+    COUNT(*) AS antal_bocker
+FROM 
+    Books
+GROUP BY category
+ORDER BY antal_bocker DESC;
+
+SELECT 
+    b.title, 
+    COUNT(l.id) AS antal_lan
+FROM Loans l
+JOIN Books b ON l.book_id = b.id
+GROUP BY b.title
+ORDER BY antal_lan DESC
+LIMIT 5;
+
+SELECT 
+    m.name, 
+    m.lats_name, 
+    COUNT(l.id) AS antal_lan
+FROM Loans l
+JOIN Members m ON l.member_id = m.id
+GROUP BY m.id
+ORDER BY antal_lan DESC;
+
+SELECT
+    AVG(return_date - loan_date) AS genomsnittliga_lanedagar
+FROM Loans
+WHERE return_date IS NOT NULL;
+
+SELECT
+    COUNT(*) AS antal_forseenade
+FROM Loans
+WHERE return_date is NULL
+    AND due_date < CURRENT_DATE;
